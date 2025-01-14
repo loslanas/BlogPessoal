519e09350a5fbba67ea4fafd5051883e89e859e7
Fernando Lana <loslanas@gmail.com>
2025-01-14 16:57:03 -0300

    Criação das funções POST, PUT e DELETE

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { PostagemService } from "../services/postagem.service";
 import { Postagem } from "../entities/postagem.entity";
 
@@ -11,8 +11,42 @@ export class PostagemController{
     ){}
     @Get()
     @HttpCode(HttpStatus.OK)
-    findall():Promise<Postagem[]> {
+    findAll():Promise<Postagem[]> {
         return  this.postagemService.findAll();
 
     }
+
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK) //parseintpipe converte de string pra int
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+        return  this.postagemService.findById(id);
+
+    }
+
+    @Get('/titulo/:titulo') //um é o caminho e o outro é a variável. 
+    @HttpCode(HttpStatus.OK) //parseintpipe não precisa pois as variaveis são string
+    findByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+        return  this.postagemService.findByTitulo(titulo);
+
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() postagem: Postagem): Promise<Postagem>{
+        return this.postagemService.create(postagem);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() postagem: Postagem): Promise<Postagem>{
+        return this.postagemService.update(postagem);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT) //parseintpipe converte de string pra int
+    delete(@Param('id', ParseIntPipe) id: number){
+        return this.postagemService.delete(id);
+
+    }
+
 }
